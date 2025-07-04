f05ef280dbcca0cd36f7260a70529caa930ee904
yeseniasanchez6 <ayesenia243@gmail.com>
2025-07-04 11:50:28 -0600

    Creating VentanaPrincipal

--- src/App.java
+++ src/App.java
@@ -1,10 +1,8 @@
-import javax.swing.JFrame;
+import screens.VentanaPrincipal;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        JFrame frame = new JFrame(" Proyecto escuela");
-        frame.setSize(500, 400);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        VentanaPrincipal ventanaPrincipal = new VentanaPrincipal("Proyecto Escuela");
+        ventanaPrincipal.setVisible(true);
     }
 }
---
+++ src/screens/VentanaPrincipal.java
@@ -0,0 +1,14 @@
+package screens;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+    public VentanaPrincipal(String title){
+        this.setTitle(title);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setSize(800, 700);
+
+    }
+    
+}

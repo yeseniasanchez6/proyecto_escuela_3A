b9b17f6c4bb6fe6a2152fb9ec4c86193f80cd6e8
yeseniasanchez6 <ayesenia243@gmail.com>
2025-07-07 10:06:07 -0600

    Adding menuBachilleratos

--- src/screens/VentanaPrincipal.java
+++ src/screens/VentanaPrincipal.java
@@ -10,6 +10,8 @@ public class VentanaPrincipal extends JFrame {
     private JMenuBar menuBar; //Barra menu
     private JMenu menuAlumnos; //menu Alumno
     private JMenuItem menuInsertarAlumnos; //Elemento Insertar
+    private JMenuItem menuVerAlumnos;
+    private JMenuItem menuEliminarAlumnos;
 
     public VentanaPrincipal(String title){
         this.setTitle(title);
@@ -29,10 +31,18 @@ public class VentanaPrincipal extends JFrame {
         menuInsertarAlumnos = new JMenuItem();
         menuInsertarAlumnos.setText("Insertar...");
 
+        //Menu ver alumnos
+        menuVerAlumnos = new JMenuItem("Ver todos los alumnos...");
+
+        //menu eliminar alumnos
+        menuEliminarAlumnos = new JMenuItem("Dar de baja alumno...");
+
         //Agregando menu
+        menuAlumnos.add(menuVerAlumnos);
         menuAlumnos.add(menuInsertarAlumnos);
+        menuAlumnos.add(menuEliminarAlumnos);
         menuBar.add(menuAlumnos);
-        
+
         this.setJMenuBar(menuBar); //menuBar Es la unica barra del JFrame
 
         pack(); //Empaquetar los componentes del JFrame

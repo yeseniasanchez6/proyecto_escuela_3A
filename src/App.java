e756d2c5cd7f3288f60c0f1b33146fe25caa613b
yeseniasanchez6 <ayesenia243@gmail.com>
2025-07-07 09:42:33 -0600

    Adding menuBar

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import screens.VentanaPrincipal;
 public class App {
     public static void main(String[] args) throws Exception {
         VentanaPrincipal ventanaPrincipal = new VentanaPrincipal("Proyecto Escuela");
+        ventanaPrincipal.setSize(800, 700);
         ventanaPrincipal.setVisible(true);
     }
 }
--- src/screens/VentanaPrincipal.java
+++ src/screens/VentanaPrincipal.java
@@ -1,13 +1,41 @@
 package screens;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class VentanaPrincipal extends JFrame {
+
+    private JMenuBar menuBar; //Barra menu
+    private JMenu menuAlumnos; //menu Alumno
+    private JMenuItem menuInsertarAlumnos; //Elemento Insertar
 
     public VentanaPrincipal(String title){
         this.setTitle(title);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(800, 700);
+        inicomponents();
+    }
+
+    private void inicomponents(){
+        //Barra de menu
+        menuBar = new JMenuBar();
+
+        //menu Alumno
+        menuAlumnos = new JMenu();
+        menuAlumnos.setText("Alumnos");
+
+        //Menu insertar
+        menuInsertarAlumnos = new JMenuItem();
+        menuInsertarAlumnos.setText("Insertar...");
+
+        //Agregando menu
+        menuAlumnos.add(menuInsertarAlumnos);
+        menuBar.add(menuAlumnos);
+        
+        this.setJMenuBar(menuBar); //menuBar Es la unica barra del JFrame
+
+        pack(); //Empaquetar los componentes del JFrame
 
     }
     
